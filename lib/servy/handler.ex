6a32609ad3a61c02a5936f23c14b1a3300e1e865
Ricardo César de Oliveira <rioliveira@bionexo.com>
2021-02-21 23:11:06 -0300

    Separate functions to other modules

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,9 +1,12 @@
 defmodule Servy.Handler do
-  require Logger
+
+  import Servy.Plugins
+  import Servy.Parse
+  import Servy.FileHandler
 
   @moduledoc "Handles HTTP requests."
 
-  @pages_path Path.expand("../../pages", __DIR__)
+  @pages_path Path.expand("pages", File.cwd!)
 
   @doc "Transforms the request into a response."
   def handle(request) do
@@ -19,56 +22,6 @@ defmodule Servy.Handler do
       |> track
       |> format_response
   end
-
-  @doc "Logs 404 requests"
-  def track(%{status: 404, path: path} = conv) do
-    Logger.warn "Warning: #{path} is on the loose!"
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{ conv | path: "/wildthings" }
-  end
-
-  #def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
-  #  %{ conv | path: "/bears/#{id}" }
-  #end
-
-  def rewrite_path(%{path: path} = conv) do
-    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
-    captures = Regex.named_captures(regex, path)
-    rewrite_path_captures(conv, captures)
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
-    %{ conv | path: "/#{thing}/#{id}" }
-  end
-
-  def rewrite_path_captures(conv, nil), do: conv
-
-  def log(conv), do: IO.inspect conv
-
-  def parse(request) do
-    [method, path, _] = 
-      request 
-        |> String.split("\n") 
-        |> List.first
-        |> String.split(" ")
-
-    %{ method: method, 
-       path: path, 
-       resp_body: "",
-       status: nil
-    }
-  end
-
-  #def route(conv) do
-    #route(conv, conv.method, conv.path)
-  #end
 
   def route(%{ method: "GET", path: "/wildthings"} = conv) do
     %{ conv | status: 200, resp_body: "Bears, Liöns, Tigers" }
@@ -138,18 +91,6 @@ defmodule Servy.Handler do
     |> handle_file(conv)
   end
 
-  def handle_file({:ok, content}, conv) do
-    %{ conv | status: 200, resp_body: content }
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{ conv | status: 404, resp_body: "File not found!" }
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{ conv | status: 500, resp_body: "File error: #{reason}" }
-  end
-
   def route(%{path: path} = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
@@ -176,14 +117,6 @@ defmodule Servy.Handler do
     }[code]
   end
 
-  def emojify(%{status: 200} = conv) do
-    emojies = String.duplicate("🎉", 5)
-    body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
-
-    %{ conv | resp_body: body }
-  end
-
-  def emojify(conv), do: conv
 
 
 end
